25ef7ff3bacfd63441497794ad4ab6cf822b8770
imArpanGhimire <arpancalma@gmail.com>
2026-02-26 23:36:03 +0545

    refactor: improve formatting and structure of Top component

--- src/Top.jsx
+++ src/Top.jsx
@@ -1,38 +1,40 @@
-import React, { useContext } from 'react'
-import { movieContext } from './App'
-
+import React, { useContext } from "react";
+import { movieContext } from "./App";
 
 const Top = () => {
-    const { moviestore, setmoviestore } = useContext(movieContext)
+  const { moviestore, setmoviestore } = useContext(movieContext);
 
-    return (
-        <>
-            <div className='flex justify-between w-[50%] mx-auto'>
-                <div >
-                    {moviestore.map((item) => {
-                        return (
-                            <>
-                                <div className='flex justify-between border-2  p-5  '>
+  return (
+    <>
+      <div className="flex justify-between w-[50%] mx-auto">
+        <div>
+          {moviestore.map((item) => {
+            return (
+              <>
+                <div className="flex justify-between border-2  p-5  ">
+                  <div className="border-2 items-center  h-fit my-auto border-red-800/80 w-[50%]">
+                    <h1 className="text-3xl font-bold ">{item.title}</h1>
+                    <p className="line-clamp-2 text-sm">{item.overview}</p>
+                    <button className="p-2 text-black border-black border-2">
+                      Watch Now
+                    </button>
+                  </div>
 
-                                    <div className='border-2 items-center  h-fit my-auto border-red-800 w-[50%]'>
-                                        <h1 className='text-3xl font-bold '>{item.title}</h1>
-                                        <p className='line-clamp-2 text-sm'>{item.overview}</p>
-                                        <button className='p-2 text-black border-black border-2'>Watch Now</button>
-                                    </div>
-
-                                    <div>
-                                        <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-                                            alt="" className='h-[230px] w-[230px]' />
-                                    </div>
-                                </div>
-                            </>
-                        )
-                    })}
+                  <div>
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                      alt=""
+                      className="h-[230px] w-[230px]"
+                    />
+                  </div>
                 </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
-            </div>
-        </>
-    )
-}
-
-export default Top
+export default Top;
